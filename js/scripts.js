2ad12cd41d4a7d0cbf80223fd2c30fb97546212b
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-06-10 08:22:12 -0700

    alert the user to the full array of numbers and include a ping for multiples of 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,33 @@
 //Business Logic//
 
+var workingArray = [];
+
 function getInput() {
-  var userNum = $('input').val();
+  var userNum = parseInt($('input').val());
   return userNum;
-}
+};
+
+function createArray(inp) {
+  for (i=1; i<=inp; i++) {
+    workingArray.push(i);
+  };
+};
+
+function pingArray(pingnum) {
+  if (pingnum % 3 === 0) {
+    return "Ping!";
+  } else {
+    return pingnum
+  };
+};
+
+//   (p=0; p<=(pingarr.length()); p++) {
+//     var pingCheck = parseInt(pingarr[p]);
+//     if (pingCheck % 3 === 0) {
+//       pingarr[p] = "ping";
+//     };
+//   };
+// };
 
 //User Input Logic//
 $(document).ready(function() {
@@ -11,6 +35,13 @@ $(document).ready(function() {
   $("form").submit(function() {
     var inputNum = getInput();
     alert(inputNum);
+    alert(typeof(inputNum));
+
+    var plainArray = createArray(inputNum);
+    alert(workingArray);
+
+    var pingedArray = workingArray.map(pingArray);
+    alert(pingedArray);
   });
 
 
