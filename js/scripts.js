09c0c81c45f0a41bcd3897a957aca67fbde7a7b2
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-06-10 09:25:37 -0700

    add a function to clear the ul each time the user enters a new number

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,16 @@
 //Business Logic//
-
 var workingArray = [];
 
 function getInput() {
   var userNum = parseInt($('input').val());
   return userNum;
 };
-
 function createArray(inp) {
+  workingArray = [];
   for (i=1; i<=inp; i++) {
     workingArray.push(i);
   };
 };
-
 function pingArray(pingnum) {
   if (pingnum % 15 === 0) {
     return "Pingpong!"
@@ -24,23 +22,20 @@ function pingArray(pingnum) {
     return pingnum;
   };
 };
-
 function makeList(elem, ind, arr) {
-  $("#outputList").append("<li>" + elem + "</li>")
+  $("#outputList").append("<li>" + elem + "</li>");
+};
+function clearList() {
+  $("#outputList").empty();
 }
-
-
 //User Input Logic//
 $(document).ready(function() {
-
   $("form").submit(function(event) {
     event.preventDefault();
+    clearList();
     var inputNum = getInput();
-
-    var plainArray = createArray(inputNum);
-
+    createArray(inputNum);
     var pingedArray = workingArray.map(pingArray);
-
     pingedArray.forEach(makeList);
   });
 });
