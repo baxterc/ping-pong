fabba72c7c0e71bc0b44c97e0f004287137929b8
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-06-10 14:00:02 -0700

    adapt pingArray function to work for both the normal ping-pong and the version with user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,28 @@
 //Business Logic//
 var workingArray = [];
-var userPingNum = 0;
-var userPongNum = 0;
-
+var userPingNum = 3;
+var userPongNum = 5;
+//Functions converting user input//
 function getInput() {
   var userNum = parseInt($('input').val());
   return userNum;
 };
-
 function getUserRange() {
   var userRng = parseInt($('#userRange').val());
   return userRng;
   alert(userRng);
 };
-
 function getUserPing() {
   var userPi = parseInt($('#userPing').val());
   return userPi;
   alert(userPin);
 };
-
 function getUserPong() {
   var userPo = parseInt($('#userPong').val());
   return userPo;
   alert(userPo);
 };
-
+//Function to create array of values//
 function createArray(inp) {
   workingArray = [];
   if (((inp / userPingNum)>=1) && ((inp / userPingNum) <= 2)) {
@@ -36,18 +33,6 @@ function createArray(inp) {
   };
 };
 function pingArray(pingnum) {
-  if (pingnum % 15 === 0) {
-    return "Pingpong!"
-  } else if (pingnum % 5 === 0) {
-    return "Ping!"
-  } else if (pingnum % 3 === 0) {
-  return "Pong!";
-  } else {
-    return pingnum;
-  };
-};
-
-function userPingArray(pingnum) {
   if ((pingnum % userPingNum === 0) && (pingnum % userPongNum ===0)) {
     return "Pingpong!"
   } else if (pingnum % userPingNum === 0) {
@@ -67,8 +52,9 @@ function clearList() {
 }
 //User Input Logic//
 $(document).ready(function() {
-
   $("#mainsubmit").submit(function(event) {
+    userPingNum = 3;
+    userPongNum =5;
     event.preventDefault();
     clearList();
     var inputNum = getInput();
@@ -76,7 +62,6 @@ $(document).ready(function() {
     var pingedArray = workingArray.map(pingArray);
     pingedArray.forEach(makeList);
   });
-
   $("#usersubmit").submit(function(event) {
     event.preventDefault();
     clearList();
@@ -84,7 +69,7 @@ $(document).ready(function() {
     userPingNum = getUserPing();
     userPongNum = getUserPong();
     createArray(inputNum);
-    var userPingedArray = workingArray.map(userPingArray);
+    var userPingedArray = workingArray.map(pingArray);
     userPingedArray.forEach(makeList);
   });
 });
